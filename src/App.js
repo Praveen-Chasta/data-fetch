6f9c18841968b1feb85d51d104d7ba89902debf1
Praveen-Chasta <praveen.chastaa@gmail.com>
2024-10-01 20:58:18 +0530

    fetch-data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
 
 function App() {
+  const [data1, setData1] = useState("");
+  const [data2, setData2] = useState("");
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+      
+        const res1 = await fetch("https://api.apis.guru/v2/list.json");
+        const reqData1 = await res1.json();
+        setData1(reqData1);
+
+      
+        const res2 = await fetch("https://api.apis.guru/v2/list.json");
+        const reqData2 = await res2.json();
+        setData2(reqData2);
+
+    
+        setMessage("Fetch Completed");
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setMessage("Error occurred during fetching");
+      }
+    };
+
+
+    fetchData();
+  }, []); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <p>JSON Data 1: {JSON.stringify(data1)}</p>
+      <p>JSON Data 2: {JSON.stringify(data2)}</p> */}
+      <p>JSON Data : {message}</p>
+    </>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+// aryan@aiplanet@.com
